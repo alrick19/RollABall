2f0fd3e1debf4d23d5985852f8bfc61e0b053e59
Alrick VINCENT <alrick.vincent@gmail.com>
2025-01-28 22:49:46 -0800

    Refactored input handling in a separate file, then added MoveBall as a listener to the onMove event

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,44 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
-
-    void Start()
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 1.1f;
+    
+    public void MoveBall(Vector2 input)
     {
-        Debug.Log("Calling the start method");
-
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        // Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        // sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
 
 
-        Debug.Log("Resultant Vector: " + inputVector);
-        Debug.Log("Resultant 3D Vector " + inputXZPlane  * ballSpeed);
+        // Debug.Log("Resultant Vector: " + inputVector);
+        // Debug.Log("Resultant 3D Vector " + inputXZPlane  * ballSpeed);
     }
 }
