702b99a6fd41a4bfdbbedb6806e61973dbec0f89
Alrick VINCENT <alrick.vincent@gmail.com>
2025-01-28 23:27:13 -0800

    Added jumping feature
    The jumping feature is an event inside InputManager.
    The way it is detected if the ball is touching the ground is through a simple boolean, which gets changed on jump, and on collision with the Plane object, using the tag "Plane".

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,15 +1,44 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private Rigidbody sphereRigidBody;
     [SerializeField] private float ballSpeed = 1.1f;
+
+    [SerializeField] private float jumpForce = 6f;
+
+    private bool isGrounded;
     
-    public void MoveBall(Vector2 input)
+    public void MoveBall(Vector3 input)
     {   
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        Vector3 inputXZPlane = new(input.x, 0, input.z);
+        sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            sphereRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        // tag Plane has been added to the Plane 3D object
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isGrounded = false;
+        }
     }
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,30 +3,37 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
-
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent OnJump = new UnityEvent();
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
-        inputVector += Vector2.up;
+        // inputVector += Vector2.up;
+        inputVector += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-        inputVector += Vector2.down;
+        // inputVector += Vector2.down;
+        inputVector -= Vector3.forward;
         }
         if (Input.GetKey(KeyCode.A))
         {
-        inputVector += Vector2.left;
+        inputVector -= Vector3.right;
         }
         if (Input.GetKey(KeyCode.D))
         {
-        inputVector += Vector2.right;
+        inputVector += Vector3.right;
         }
         
-
         OnMove?.Invoke(inputVector);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJump?.Invoke();
+        }
     }
 }
