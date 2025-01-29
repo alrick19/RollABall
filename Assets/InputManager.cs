1ba93014d474f7b960d49e3dbd0ffaa40253c78f
Alrick VINCENT <alrick.vincent@gmail.com>
2025-01-28 23:03:13 -0800

    Added materials for the sphere, walls and plane

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,12 +5,6 @@ public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -31,6 +25,7 @@ public class InputManager : MonoBehaviour
         {
         inputVector += Vector2.right;
         }
+        
 
         OnMove?.Invoke(inputVector);
     }
